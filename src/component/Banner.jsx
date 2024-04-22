00324c1e4b121822f3c02f2bc6d1a907cf1e423c
Toshiakia <132526598+Toshiakia@users.noreply.github.com>
2024-04-22 18:13:26 +1200

    add banner.css and banner.jsx
    i have add banner of web page and css stayle

---
+++ src/component/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css'; // Ensure the path is correct based on your file structure
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="banner-bg">
+                <h2><b>EASY/ DELICIOUS/ HEALTHY</b></h2>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
