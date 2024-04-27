01962970de9293bbe92f76dcebbbe9f042e0f14e
Latte123456 <inayumi7@gmail.com>
2024-04-27 17:54:06 +1200

    API for location
    - Created a travelAdvisoryAPI for Google Maps where it retrieves location of the maps.

---
+++ src/api/travelAdvisorAPI.js
@@ -0,0 +1,26 @@
+/* eslint-disable consistent-return */
+import axios from 'axios';
+
+//retrieve places 
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    //GET request to travel-advisor API
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': 'b10a322c33msh72c0cd5f51f337bp16c40ejsnc60455e192eb',
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
