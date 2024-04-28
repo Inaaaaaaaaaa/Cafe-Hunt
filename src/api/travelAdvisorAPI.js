ea56ec723ec612b6aa7d6a2c9f81954e05bf88c6
Latte123456 <inayumi7@gmail.com>
2024-04-29 11:23:28 +1200

    Update travelAdvisorAPI.js
    - Remove weather (not needed because we are not tracking the weather, only the location)

--- src/api/travelAdvisorAPI.js
+++ src/api/travelAdvisorAPI.js
@@ -24,22 +24,3 @@ export const getPlacesData = async (type, sw, ne) => {
   }
 };
 
-//retrieve weather data based on area 
-export const getWeatherData = async (lat, lng) => {
-    try {
-      if (lat && lng) {
-        const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
-          params: { lat, lon: lng },
-          headers: {
-            'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
-            'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-          },
-        });
-  
-        return data;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
